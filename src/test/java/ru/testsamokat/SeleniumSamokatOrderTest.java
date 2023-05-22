25068a1d06cadc0179a9b088eed985ca3f8cfcb1
Ivan Sokolov <lookbook.iv@gmail.com>
2023-05-22 15:53:56 +0200

    Sprint_4_v1

---
+++ src/test/java/ru/testsamokat/SeleniumSamokatOrderTest.java
@@ -0,0 +1,35 @@
+package ru.testsamokat;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SeleniumSamokatOrderTest extends Basetest {
+
+
+    @Test
+    public void testOrderSamokatWithHeader() throws InterruptedException {
+
+        pages.clickHeaderOrderButton();
+        pages.setMetro();
+        pages.fillFirstName("тутМогла");
+        pages.fillSecondName("БытьВаша");
+        pages.fillAddress("1-ая Реклама");
+        pages.fillPhoneNumber("+7911111111");
+        pages.clickNextButton();
+        pages.setDateCalendar();
+        pages.setTimeForUse();
+        pages.selectColourBlack();
+        pages.clickOnDoOrder();
+        pages.clickOnYesButton();
+        Assert.assertTrue(pages.getTextAfterOrder());
+
+    }
+
+    @Test
+    public void checkListFAQ() {
+
+
+    }
+
+
+}
